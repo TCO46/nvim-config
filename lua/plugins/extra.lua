2914eee2b1ac8a0ab79a4efccc1c06a0dcc6f007
TCO46 <patohru2006@gmail.com>
2025-03-10 16:40:29 +0700

    update some plugins

--- lua/plugins/extra.lua
+++ lua/plugins/extra.lua
@@ -1,4 +1,39 @@
 return {
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'ThePrimeagen/vim-be-good',
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+  },
+  {
+        'nvim-tree/nvim-web-devicons',
+        lazy = false,
+    },
+    {
+        'akinsho/toggleterm.nvim',
+        keys = { { '<C-\\>', '<cmd>ToggleTerm<CR>', 'Toggle terminal' } },
+        opts = {
+            open_mapping = [[<c-\>]],
+            winbar = { enabled = true, },
+        },
+    },
+    {
+        'onsails/lspkind.nvim',
+        opts = {
+            preset = 'codicons',
+            mode = 'symbol',
+            symbol_map = {
+                Text = '',
+                -- Method = '',
+                -- Function = '',
+                Constructor = '',
+            },
+        },
+        config = function(_, opts)
+            require('lspkind').init(opts)
+        end,
+    },
 }
