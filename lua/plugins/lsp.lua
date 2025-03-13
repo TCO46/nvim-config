b59e32c99892611c8ca9ba4a28bd7187f5030370
TCO46 <patohru2006@gmail.com>
2025-03-13 11:35:59 +0700

    fix TS tool LSP

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -216,9 +216,6 @@ return {
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
             require('lspconfig')[server_name].setup(server)
           end,
@@ -226,5 +223,9 @@ return {
       }
     end,
   },
-  {"typescript-tools.nvim"}
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+  }
 }
