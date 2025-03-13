a868007c57df5c5bfc706aa5e28395f06f1b733b
TCO46 <patohru2006@gmail.com>
2025-03-13 16:48:54 +0700

    change reletive num color

--- lua/options.lua
+++ lua/options.lua
@@ -53,5 +53,5 @@ vim.opt.scrolloff = 10
 
 vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#51B3EC', bold = true })
 vim.api.nvim_set_hl(0, 'LineNr', { fg = 'white', bold = true })
-vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#FB508F', bold = true })
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#51B3EC', bold = true })
 
